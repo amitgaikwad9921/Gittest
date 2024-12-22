f4b63200d5e3b4627ae966ea107cc194b72e71df
dac <dac@students.acts.in>
2024-12-22 16:06:50 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,11 @@
+
+class App{
+	
+	public static void main(String[]args){
+			System.out.println("Program to print first 100 odd numbers");
+		
+		for(int i=1;i<=100;i++) {
+			System.out.print(i+" ");
+		}
+	}
+}
